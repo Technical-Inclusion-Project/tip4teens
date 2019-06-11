44509d95af28a37f3bbc29443a38718b496ab064
Harry Ho <harry.ho@gstv.com>
2019-06-11 18:33:26 -0400

    init()

---
+++ aarion/scripts.js
@@ -0,0 +1,26 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+
+$('.trigger').on('click', function () {
+    $(this).toggleClass('active').next().toggleClass('active');
+});
